4c095b569e915a42b68e0e49a213087cbf72f9b8
theeramet0793 <59278790+theeramet0793@users.noreply.github.com>
2021-02-02 14:01:11 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { myComponent } from "./component/myComponent";
-const 
+const [myCourses] = useState([]);
 function App() {
   const persons = [
     {
@@ -64,9 +64,11 @@ function App() {
             <td>male</td>
             <td>20</td>
           </tr>
-          {myCourses.map(item => {
-          return <myComponent name ={item.name} grd = {item.gender} crd ={item.age}  />
-        })}
+          {myCourses.map((item) => {
+            return (
+              <myComponent name={item.name} grd={item.gender} crd={item.age} />
+            );
+          })}
         </tbody>
       </table>
     </div>
